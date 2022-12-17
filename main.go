9da9d1d261b84fd4f27a03f9c9131609eb6e8a80
Gary Bowers <gary@bowers1.com>
2022-12-17 15:10:54 +0000

    Add server

--- main.go
+++ main.go
@@ -11,13 +11,45 @@ func main() {
 	fmt.Println("Where Am I? v0.0.1")
 	fmt.Println("==================")
 
-	clusterName, _ := getCurrentCluster()
-	log.Println(clusterName)
-
+	http.HandleFunc("/", root)
+	log.Println("Listening on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
 
-func getCurrentCluster() (clusterName string, err error) {
+func root(w http.ResponseWriter, req *http.Request) {
+	clusterName, _ := getCurrentClusterName()
+	clusterLocation, _ := getCurrentClusterLocation()
+
+	log.Println(clusterName + " -- " + clusterLocation)
+
+	fmt.Fprintf(w, clusterName)
+	fmt.Fprintf(w, clusterLocation)
+}
+
+func getCurrentClusterName() (clusterName string, err error) {
 	url := "http://metadata/computeMetadata/v1/instance/attributes/cluster-name"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("Metadata-Flavor", "Google")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(b), nil
+}
+
+func getCurrentClusterLocation() (clusterLocation string, err error) {
+	url := "http://metadata/computeMetadata/v1/instance/attributes/cluster-location"
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal(err)
